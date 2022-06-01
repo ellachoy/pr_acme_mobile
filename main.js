20b7b6f27e99945b03e58b9cc54f90d78b983c4a
ella choy <egal21de@gmail.com>
2022-06-01 21:06:45 +0200

    renew_footer

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-const navbarMenu=document.querySelector('.navbar__menu');
-const hamburger=document.querySelector('.navbar__toggle-btn');
-const closeBtn= document.querySelector(".closeBtn");
-const subLink= document.querySelector('.modal_sub');
-const subMenu=document.querySelector('.dropdown-content');
-hamburger.addEventListener('click',()=>{
-    console.log('toggle test')
-    navbarMenu.classList.toggle('open')
-    // hamburger.classList.add("hidden")
-})
-closeBtn.addEventListener('click',()=>{
-    console.log('close test');
-    navbarMenu.classList.add("hidden");
-    // hamburger.classList.add('open');
-})
+// console.log("hallo main js")
+
+// //-------------------  menubar test1  in mobile version---------------------
+// const navbar= document.getElementById("navbar")
+// const navbarMenu= document.querySelector(".navbar__menu");
+// const hamburger= document.querySelector(".navbar__toggle-btn");
+// const closeBtn= document.querySelector(".closeBtn");
+// const subShow= document.querySelector('.modal_sub');
+// const subMenu=document.querySelector('.sub__menu');
+
+// hamburger.addEventListener('click',()=>{
+//     console.log('toggle test')
+//     navbarMenu.classList.toggle('open')
+//     // hamburger.classList.add("hidden")
+// })
+// // closeBtn.addEventListener('click',()=>{
+// //     console.log('close test');
+// //     navbarMenu.classList.add("hidden");
+// //     hamburger.classList.add('open');
+// // })
+// // subShow.addEventListener('click',()=>{
+// //     console.log('test submenu')
+// //     subMenu.classList.toggle('open');
+// // })
